a86beed563579a20a38643772b79ca81ccc9b0e2
Laku <llakujack@outlook.com>
2021-04-13 17:48:57 +1000

    User implementation and userPermissions

---
+++ src/Organization.java
@@ -0,0 +1,16 @@
+/**
+ * This class initialises an Organization and there assets
+ *
+ * @author Laku Jackson
+ */
+
+public class Organization {
+
+
+    public Organization() {
+    }
+
+
+//this song slaps
+
+}
