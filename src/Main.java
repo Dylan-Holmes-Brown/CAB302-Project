fee63ed3e3a85635473ac0d725f74542d0de032b
Dylan Holmes-Brown <dylanholmesbrown@gmail.com>
2021-04-14 17:29:06 +1000

    Started Create Table method, ran into issues with converting variable types into sql appropriate strings.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import sql.CreateDatabase;
+import sql.Statement;
+
 import java.sql.*;
 
+/**
+ * This class initilises the database
+ *
+ * @author Dylan Holmes-Brown
+ */
 public class Main {
+
     public static void main(String[] args) throws Exception{
         DriverManager.registerDriver(new org.sqlite.JDBC());
         CreateDatabase.build();
+        System.out.println(CreateDatabase.CreateTable(User.class)); // Test code to see if sql query is correct
+
     }
 }
