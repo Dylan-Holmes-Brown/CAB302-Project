a6fd4e04565a97572861d2626241ecfd8d1fc30d
Dylan Holmes-Brown <dylanholmesbrown@gmail.com>
2021-06-09 19:57:18 +1000

    user login documentation completed

--- src/Views/loginGui.java
+++ src/Views/loginGui.java
@@ -13,7 +13,7 @@ import java.awt.event.WindowEvent;
 import java.io.Serializable;
 
 /**
- * Initialises the user interface for the adding UI. Button listeners are included
+ * Initialises the user interface for the Asset Trading System. Displays login GUI, all listeners are included
  * as sub-classes of this class
  *
  * @author Vipin Vijay
@@ -24,15 +24,17 @@ import java.io.Serializable;
 public class loginGui extends JFrame implements Serializable {
 
     private static final long serialVersionUID = 100L;
+    // Jswing Variables
     private static JLabel userLabel;
     private static JTextField userField;
     private static JLabel passLabel;
     private static JPasswordField passField;
-
     private JRadioButton memberButton;
     private JRadioButton adminButton;
     private ButtonGroup radioGroup;
     private JButton login;
+
+    // Database accessor
     private UserData data;
 
     /**
@@ -50,10 +52,10 @@ public class loginGui extends JFrame implements Serializable {
             data.add(admin);
         }
 
-        // Initialise the UI and listen for a button click
+        // Initialise the UI and listen for a Button press or window close
         initUI();
-        login.addActionListener(new loginGui.ButtonListener());
-        addWindowListener(new loginGui.ClosingListener());
+        addButtonListeners(new ButtonListener());
+        addWindowListener(new ClosingListener());
 
         // Set up the frame
         setTitle("Asset Trading System - User Login");
@@ -64,23 +66,31 @@ public class loginGui extends JFrame implements Serializable {
     }
 
     /**
-     * Initialises the UI placing the panels in a box layout with vertical
+     * Initialises the UI placing the panels in a box layout with Y Axis
      * alignment spacing each panel.
      */
     private void initUI() {
+        // Create a container for the panels and set the layout
         Container contentPane = this.getContentPane();
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+
+        // Add panels to container with padding
         contentPane.add(Box.createVerticalStrut(20));
         contentPane.add(makeUserDetailsPanel());
         contentPane.add(Box.createVerticalStrut(5));
         contentPane.add(makeButtonsPanel());
         contentPane.add(Box.createVerticalStrut(20));
-        contentPane.add(makeCreateUserPanel());
+        contentPane.add(makeLoginPanel());
         contentPane.add(Box.createVerticalStrut(20));
     }
 
+    /**
+     * Create a JPanel with fields and labels for username and password
+     *
+     * @return the JPanel created
+     */
     private JPanel makeUserDetailsPanel() {
-
+        // Initialise the JPanel
         JPanel userPanel = new JPanel();
         GroupLayout layout = new GroupLayout(userPanel);
         userPanel.setLayout(layout);
@@ -89,11 +99,11 @@ public class loginGui extends JFrame implements Serializable {
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
 
-        // Label of fields
+        // Initialise field labels
         userLabel = new JLabel("Username");
         passLabel = new JLabel("Password");
 
-        // Text Fields
+        // Initialise text fields
         userField = new JTextField(30);
         userField.setPreferredSize(new Dimension(30, 20));
         passField = new JPasswordField(30);
@@ -118,44 +128,74 @@ public class loginGui extends JFrame implements Serializable {
 
     }
 
-    private Component makeButtonsPanel() {
+    /**
+     * Create a JPanel of radio buttons for the Member and Admin account types
+     *
+     * @return the button panel created
+     */
+    private JPanel makeButtonsPanel() {
+        // Initialise the JPanel
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
 
-        // Label Buttons
+        // Initialise the radio buttons
         memberButton = new JRadioButton("Member");
         adminButton = new JRadioButton("Admin");
         radioGroup = new ButtonGroup();
 
-        // Create and space Buttons
+        // Add buttons to the panel with padding
         buttonPanel.add(Box.createHorizontalStrut(30));
         buttonPanel.add(memberButton);
         buttonPanel.add(Box.createHorizontalStrut(30));
         buttonPanel.add(adminButton);
 
+        // Add buttons to a button group
         radioGroup.add(memberButton);
         radioGroup.add(adminButton);
 
         return buttonPanel;
     }
 
-    private JPanel makeCreateUserPanel() {
-        JPanel createUserPanel = new JPanel();
-        createUserPanel.setLayout(new BoxLayout(createUserPanel, BoxLayout.X_AXIS));
+    /**
+     * Create a JPanel for the login button
+     *
+     * @return the button panel created
+     */
+    private JPanel makeLoginPanel() {
+        // Initialise the JPanel
+        JPanel loginPanel = new JPanel();
+        loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.X_AXIS));
 
-        // Create Button, add to panel and add spacing
+        // Initialise the button and add it to the panel
         login = new JButton("Login");
-        createUserPanel.add(Box.createHorizontalStrut(50));
-        createUserPanel.add(login);
-        createUserPanel.add(Box.createHorizontalStrut(50));
-        return createUserPanel;
+        loginPanel.add(Box.createHorizontalStrut(50));
+        loginPanel.add(login);
+        loginPanel.add(Box.createHorizontalStrut(50));
+        return loginPanel;
     }
 
+    /**
+     * Adds a listener to the buttons
+     *
+     * @param listener the listener for the buttons to use
+     */
+    private void addButtonListeners(ActionListener listener) {
+        login.addActionListener(listener);
+    }
+
+    /**
+     * Handles events for the login button on the UI.
+     *
+     * @author Vipin Vijay
+     * @author Dylan Holmes-Brown
+     * @author Laku Jackson
+     */
     private class ButtonListener implements ActionListener {
         /**
          * @see ActionListener#actionPerformed(ActionEvent)
          */
         public void actionPerformed(ActionEvent e) {
+            // Get the button pressed and go to corresponding method
             JButton source = (JButton) e.getSource();
             if (source == login) {
                 createPressed();
@@ -167,13 +207,13 @@ public class loginGui extends JFrame implements Serializable {
          * or display error
          */
         private void createPressed() {
-            String accountType = "";
+            String accountType = null;
 
-            // If all fields are filled in continue
+            // Check if all fields are filled in
             if (userField.getText() != null && !userField.getText().equals("")
                     && !passField.equals("") && (memberButton.isSelected() || adminButton.isSelected())) {
 
-//                // Depending on radio button selected choose account type
+//                // Depending on radio button selected, choose account type
                 if (memberButton.isSelected()) {
                     accountType = "Member";
                 }
@@ -181,27 +221,37 @@ public class loginGui extends JFrame implements Serializable {
                     accountType = "Admin";
                 }
 
-                // ver
+                // Get the user based on the input and initialise a user object of all fields inputted
                 User match = data.get(userField.getText());
                 User input = new User(userField.getText(), HashPassword.hashPassword(String.valueOf(passField.getPassword())), accountType);
+
+                // Check that a user was found
                 if (match != null) {
+                    // Check that all fields match the user grabbed from the database
                     if (match.getUsername().equals(input.getUsername()) && match.getPassword().equals(input.getPassword()) &&
                             match.getAccountType().equals(input.getAccountType())) {
+                        // Check if the user is an Admin
                         if (match.getAccountType().equals("Admin")) {
+                            // persist the data, close the window and continue to the admin options frame
                             data.persist();
                             dispose();
                             new adminOptions(match);
                         }
+
+                        // Check if the user is a Member
                         else if (match.getAccountType().equals("Member")) {
+                            // persist the data, close the window and continue to the member options frame
                             data.persist();
                             dispose();
                             new userOptions(match);
                         }
                     }
+                    // Fields entered do not match a user in the database
                     else {
                         JOptionPane.showMessageDialog(null, "Incorrect Credentials, Please Try Again", "Credentials Error", JOptionPane.ERROR_MESSAGE);
                     }
                 }
+                // Username entered cannot be found in the database
                 else {
                     JOptionPane.showMessageDialog(null, String.format("User '%s' cannot be found, Please Try Again", userField.getText()), "Credentials Error", JOptionPane.ERROR_MESSAGE);
                 }
@@ -223,6 +273,7 @@ public class loginGui extends JFrame implements Serializable {
          * @see WindowAdapter#windowClosing(WindowEvent)
          */
         public void windowClosing(WindowEvent e) {
+            // persist data and stop the application
             data.persist();
             System.exit(0);
         }
