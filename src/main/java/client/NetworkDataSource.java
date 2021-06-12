1e8a6d123cb9b4a950646b14ed8b7f0675ab53e5
Dylan Holmes-Brown <dylanholmesbrown@gmail.com>
2021-06-12 15:23:34 +1000

    network data source documentation complete

--- src/main/java/client/NetworkDataSource.java
+++ src/main/java/client/NetworkDataSource.java
@@ -26,7 +26,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     private ObjectInputStream inputStream;
 
     /**
-     *
+     * Constructor initialises the connection to the server
      */
     public NetworkDataSource() {
         Properties properties = new Properties();
@@ -71,7 +71,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
      */
     @Override
     public void addAssetType(AssetType asset) {
-        // asset object is null
+        // Check asset object is null
         if (asset == null)
             throw new IllegalArgumentException("Asset cannot be null");
         try {
@@ -93,7 +93,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public AssetType getAsset(String assetName) {
         try {
-            // Tell main.server - expect asset name, and send the details
+            // Tell server - expect asset name, and send the details
             outputStream.writeObject(Commands.GET_ASSET);
             outputStream.writeObject(assetName);
 
@@ -115,7 +115,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public int getAssetSize() {
         try {
-            // No parameters
+            // Tell server - No paramenters and send asset size
             outputStream.writeObject(Commands.GET_ASSET_SIZE);
             outputStream.flush();
 
@@ -134,7 +134,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public void deleteAssetType(String assetName) {
         try {
-            // Tell main.server - expect asset name, and send the details
+            // Tell server - expect asset name, and remove the asset
             outputStream.writeObject(Commands.DELETE_ASSET);
             outputStream.writeObject(assetName);
             outputStream.flush();
@@ -150,6 +150,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public Set<String> AssetNameSet() {
         try {
+            // Tell server - get the name set of the asset table
             outputStream.writeObject(Commands.GET_ASSET_NAME_SET);
             outputStream.flush();
             return (Set<String>)  inputStream.readObject();
@@ -169,11 +170,12 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
      */
     @Override
     public void addOrg(Organisation org) {
+        // Check org object is not null
         if (org == null)
             throw new IllegalArgumentException("Organisation cannot be null");
 
         try {
-            // Tell the main.server to expect a organisation's details
+            // Tell the server to expect a organisation's details
             outputStream.writeObject(Commands.ADD_ORG);
 
             // Send organisation data
@@ -190,6 +192,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public void deleteOrg(String name) {
         try {
+            // Tell server - expect organisation name, and delete
             outputStream.writeObject(Commands.DELETE_ORG);
             outputStream.writeObject(name);
             outputStream.flush();
@@ -204,6 +207,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public void addCredits(String name, int credits) {
         try {
+            // Tell server - expect organisation name and credits and add credits amount
             outputStream.writeObject(Commands.ADD_CREDITS);
             outputStream.writeObject(name);
             outputStream.writeObject(credits);
@@ -219,6 +223,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public void removeCredits(String name, int credits) {
         try {
+            // Tell server - expect organisation name and credits and remove credits amount
             outputStream.writeObject(Commands.REMOVE_CREDITS);
             outputStream.writeObject(name);
             outputStream.writeObject(credits);
@@ -232,12 +237,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
      * @see OrganisationDataSource#addQuantity(String, String, int)
      */
     @Override
-    public void addQuantity(String name, String asset, int credits) {
+    public void addQuantity(String name, String asset, int quantity) {
         try {
+            // Tell server - expect organisation name, asset and quantity and add quantity amount
             outputStream.writeObject(Commands.ADD_QUANTITY);
             outputStream.writeObject(name);
             outputStream.writeObject(asset);
-            outputStream.writeObject(credits);
+            outputStream.writeObject(quantity);
             outputStream.flush();
         } catch (IOException e) {
             e.printStackTrace();
@@ -250,6 +256,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public void removeQuantity(String name, String asset, int credits) {
         try {
+            // Tell server - expect organisation name, asset and quantity and remove quantity amount
             outputStream.writeObject(Commands.REMOVE_QUANTITY);
             outputStream.writeObject(name);
             outputStream.writeObject(asset);
@@ -266,12 +273,12 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public Organisation getOrg(Integer id) {
         try {
-            // Tell main.server to expect a organisations name, and send the details
+            // Tell server to expect a organisations name, and send the details
             outputStream.writeObject(Commands.GET_ORG);
             outputStream.writeObject(id);
             outputStream.flush();
 
-            // Read the organisation's details from the main.server
+            // Read the organisation's details from the server
             return (Organisation) inputStream.readObject();
         } catch (IOException | ClassNotFoundException  | ClassCastException e) {
             e.printStackTrace();
@@ -285,6 +292,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public Set<String> orgAssetSet(String organisation) {
         try {
+            // Tell server - expect organisation name, and send the details
             outputStream.writeObject(Commands.GET_ORG_ASSET_SET);
             outputStream.writeObject(organisation);
             outputStream.flush();
@@ -301,6 +309,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public int getOrgSize() {
         try {
+            // Tell server - send the size of the table
             outputStream.writeObject(Commands.GET_ORG_SIZE);
             outputStream.flush();
 
@@ -319,6 +328,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public Set<Integer> OrgNameSet() {
         try {
+            // Tell server - Send the id of all organisations
             outputStream.writeObject(Commands.GET_ORG_NAME_SET);
             outputStream.flush();
             return (Set<Integer>) inputStream.readObject();
@@ -336,12 +346,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
      */
     @Override
     public void addUser(User user) {
+        // Check user object is not null
         if (user == null) {
             throw new IllegalArgumentException("User cannot be null");
         }
 
         try {
-            // Tell the main.server to expect a user's details
+            // Tell the server to expect a user's details
             outputStream.writeObject(Commands.ADD_USER);
 
             // Send the user data
@@ -358,7 +369,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public void updatePassword(String username, String password) {
         try {
-            // Tell the main.server to expect a user's details
+            // Tell the server to expect a user's details
             outputStream.writeObject(Commands.UPDATE_PASSWORD);
 
             // Send the data
@@ -376,7 +387,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public User getUser(String username) {
         try {
-            // Tell the main.server to expect the user's username, and send the details
+            // Tell the server to expect the user's username, and send the details
             outputStream.writeObject(Commands.GET_USER);
             outputStream.writeObject(username);
             outputStream.flush();
@@ -395,10 +406,11 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public int getUserSize() {
         try {
+            // Tell server - to send the user size
             outputStream.writeObject(Commands.GET_USER_SIZE);
             outputStream.flush();
 
-            // Read the user details from the main.server
+            // Read the user details from the server
             return inputStream.readInt();
         } catch (IOException | ClassCastException e) {
             e.printStackTrace();
@@ -412,6 +424,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public void deleteUser(String username) {
         try {
+            // Tell server - expect username, and delete the user
             outputStream.writeObject(Commands.DELETE_USER);
             outputStream.writeObject(username);
             outputStream.flush();
@@ -426,6 +439,7 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     @Override
     public Set<String> UsernameSet() {
         try {
+            // Tell server - get the name of all users
             outputStream.writeObject(Commands.GET_USER_NAME_SET);
             outputStream.flush();
             return (Set<String>) inputStream.readObject();
@@ -438,13 +452,18 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     /*
     Current Trade
      */
+    /**
+     * @see CurrentDataSource#addTrade(Trade)
+     */
     @Override
     public void addTrade(Trade trades) {
+        // Check trades object is not null
         if (trades == null) {
             throw new IllegalArgumentException("Trade cannot be null");
         }
 
         try {
+            // Tell server - expect trades object, and send the details
             outputStream.writeObject(Commands.ADD_TRADE);
 
             outputStream.writeObject(trades);
@@ -454,10 +473,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see CurrentDataSource#getCurrentTrade(Integer)
+     */
     @Override
     public Trade getCurrentTrade(Integer id) {
         try {
-            // Tell the main.server to expect the current trades id, and send the details
+            // Tell the server to expect the current trades id, and send the details
             outputStream.writeObject(Commands.GET_CURRENT_TRADE);
             outputStream.writeObject(id);
             outputStream.flush();
@@ -470,9 +492,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see CurrentDataSource#typeSet(String)
+     */
     @Override
     public Set<Integer> typeSet(String type) {
         try {
+            // Tell server - expect trade type, and send all trades of that type
             outputStream.writeObject(Commands.GET_BUY_SELL);
             outputStream.writeObject(type);
             outputStream.flush();
@@ -483,9 +509,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see CurrentDataSource#orgSet(String)
+     */
     @Override
     public Set<Integer> orgSet(String org) {
         try {
+            // Tell server - expect organisation name, and send all trades of that name
             outputStream.writeObject(Commands.GET_ORGTRADE);
             outputStream.writeObject(org);
             outputStream.flush();
@@ -496,9 +526,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see CurrentDataSource#getCurrentSize()
+     */
     @Override
     public int getCurrentSize() {
         try {
+            // Tell server - send the size of the trade table
             outputStream.writeObject(Commands.GET_TRADE_SIZE);
             outputStream.flush();
 
@@ -509,9 +543,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see CurrentDataSource#deleteTrade(int)
+     */
     @Override
     public void deleteTrade(int id) {
         try {
+            // Tell server - expect trade id, and delete trade of that id
             outputStream.writeObject(Commands.DELETE_TRADE);
             outputStream.writeObject(id);
             outputStream.flush();
@@ -520,9 +558,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see CurrentDataSource#idSet()
+     */
     @Override
     public Set<Integer> idSet() {
         try {
+            // Tell server - send all trades id
             outputStream.writeObject(Commands.GET_TRADE_NAME_SET);
             outputStream.flush();
             return (Set<Integer>) inputStream.readObject();
@@ -535,13 +577,18 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
     /*
     Trade History
      */
+    /**
+     * @see TradeHistoryDataSource#addTradeHistory(Trade)
+     */
     @Override
     public void addTradeHistory(Trade trades) {
+        // Check trades object is null
         if (trades == null) {
             throw new IllegalArgumentException("Trade cannot be null");
         }
 
         try {
+            // Tell server - expect trade object, and add trade
             outputStream.writeObject(Commands.ADD_TRADE_HISTORY);
 
             outputStream.writeObject(trades);
@@ -551,9 +598,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see TradeHistoryDataSource#getHistorySize()
+     */
     @Override
     public int getHistorySize() {
         try {
+            // Tell server - send size of trade history table
             outputStream.writeObject(Commands.GET_TRADE_HISTORY_SIZE);
             outputStream.flush();
 
@@ -564,9 +615,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see TradeHistoryDataSource#historySet()
+     */
     @Override
     public Set<Integer> historySet() {
         try {
+            // Tell server - send all trades id
             outputStream.writeObject(Commands.GET_TRADE_HISTORY_ID_SET);
             outputStream.flush();
             return (Set<Integer>) inputStream.readObject();
@@ -576,9 +631,13 @@ public class NetworkDataSource implements AssetTypeDataSource, OrganisationDataS
         }
     }
 
+    /**
+     * @see TradeHistoryDataSource#assetSet(String)
+     */
     @Override
     public Set<Integer> assetSet(String asset) {
         try {
+            // Tell server - expect asset, and send all trades of the asset
             outputStream.writeObject(Commands.GET_TRADE_ASSETS);
             outputStream.writeObject(asset);
             outputStream.flush();
